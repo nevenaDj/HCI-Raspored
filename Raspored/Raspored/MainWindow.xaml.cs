5f69f7422ca9d6dbb539e9cfb40dee2ef6bd8bd4
Sabinomi <Sabinomi@DESKTOP-7V6VGAR>
2017-06-03 00:06:04 +0200

    rasporedi

--- Raspored/Raspored/MainWindow.xaml.cs
+++ Raspored/Raspored/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,7 +26,34 @@ namespace Raspored
         public MainWindow()
         {
             InitializeComponent();
+            FileStream f = new FileStream("../../Save/recent.txt", FileMode.OpenOrCreate);
+            f.Close();
+
+            RegexOptions options = RegexOptions.None;
+            Regex regex = new Regex("[\r\n]{3,}", options);
+            string recentText = File.ReadAllText("../../Save/recent.txt");
+
+            string[] tekst = recentText.Split('\n');
+            string recentFile = tekst[0];
+            try
+            {
+                
+                string fileText = File.ReadAllText(recentFile);
+                //MessageBox.Show(recentFile);
+                Prozor1.Visibility = Visibility.Visible;
+                Prozor2.Visibility = Visibility.Hidden;
+                Raspored_Button.IsEnabled = true;
+            } catch (Exception e)
+            {
+                Prozor2.Visibility = Visibility.Visible;
+                Prozor1.Visibility = Visibility.Hidden;
+                Raspored_Button.IsEnabled = false;
+                //MessageBox.Show("error");
+            }
         }
+
+        private Model.Raspored raspored;
+
 
         private void Ucionice_Click(object sender, RoutedEventArgs e)
         {
@@ -42,5 +72,60 @@ namespace Raspored
         {
             this.Focus();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Raspored dokument (.rsp)|*.rsp";
+
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                String filename = saveFileDialog.FileName;
+                FileStream f = new FileStream(filename, FileMode.Create);
+                f.Close();
+                saveRecent(filename);
+                Prozor1.Visibility = Visibility.Visible;
+                Prozor2.Visibility = Visibility.Hidden;
+                Raspored_Button.IsEnabled = true;
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.DefaultExt = ".rsp";
+            openFileDialog.Filter = "Raspored dokument (.rsp)|*.rsp";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                String filename = openFileDialog.FileName;
+                saveRecent(filename);
+                Prozor1.Visibility = Visibility.Visible;
+                Prozor2.Visibility = Visibility.Hidden;
+                Raspored_Button.IsEnabled = true;
+            }
+        }
+
+        void saveRecent(String filename)
+        {
+            String file_tekst = filename+"\n";
+            FileStream f = new FileStream("../../Save/recent.txt", FileMode.OpenOrCreate);
+            f.Close();
+
+            RegexOptions options = RegexOptions.None;
+            Regex regex = new Regex("[\r\n]{3,}", options);
+            string recentText = File.ReadAllText("../../Save/recent.txt");
+
+            foreach (string tekst in recentText.Split('\n'))
+            {
+                if (tekst != filename)
+                    file_tekst += tekst + "\n";
+            }
+
+            StreamWriter f_save = new StreamWriter("../../Save/recent.txt");
+            f_save.Write(file_tekst);
+            f_save.Close();
+        }
+        
     }
 }
